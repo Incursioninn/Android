dc98d289890e0cdfd3b14cf08803eb68e62d7cda
Incursioninn <lastacchs@gmail.com>
2022-10-02 21:06:10 +0300

    Modified with viewBind

--- app/src/main/java/com/example/lab1_kt/HelloActivity.kt
+++ app/src/main/java/com/example/lab1_kt/HelloActivity.kt
@@ -3,41 +3,33 @@ package com.example.lab1_kt
 import android.app.Activity
 import android.graphics.Color
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
+import com.example.lab1_kt.databinding.ActivityHelloactBinding
 import kotlin.random.Random
 
 class HelloActivity : Activity() {
 
-    private lateinit var buttonInc : Button
-    private lateinit var buttonDec : Button
-    private lateinit var textView: TextView
+    private lateinit var binding : ActivityHelloactBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.actibity_helloact)
+        binding = ActivityHelloactBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         var counter = 0
 
-        buttonInc = findViewById(R.id.buttonLeft)
-        buttonDec = findViewById(R.id.buttonRight)
-        textView =  findViewById(R.id.textView1)
+        
 
-
-
-
-
-        buttonInc.setOnClickListener {
+        binding.buttonLeft.setOnClickListener {
             counter ++
-            textView.text = "$counter"
-            textView.setTextColor(Color.rgb(Random.nextInt(0,255),Random.nextInt(0,255),Random.nextInt(0,255)))
+            binding.textView1.text = "$counter"
+            binding.textView1.setTextColor(Color.rgb(Random.nextInt(0,255),Random.nextInt(0,255),Random.nextInt(0,255)))
 
         }
-        buttonDec.setOnClickListener {
+        binding.buttonRight.setOnClickListener {
             counter --
-            textView.text = "$counter"
-            textView.setTextColor(Color.rgb(Random.nextInt(0,255),Random.nextInt(0,255),Random.nextInt(0,255)))
+            binding.textView1.text = "$counter"
+            binding.textView1.setTextColor(Color.rgb(Random.nextInt(0,255),Random.nextInt(0,255),Random.nextInt(0,255)))
         }
 
 
